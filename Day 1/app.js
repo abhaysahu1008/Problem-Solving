e72a935365a731d7549e7b074a87a3cd5bbd78b7
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-04-19 18:04:04 +0530

    pattern 1

---
+++ Day 1/app.js
@@ -0,0 +1,16 @@
+function starPat(n) {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+
+    for (let j = 1; j <= n + i - 1; j++) {
+      if (j <= n - i) {
+        str += " ";
+      } else {
+        str += "*";
+      }
+    }
+    console.log(str);
+  }
+}
+
+starPat(5);
